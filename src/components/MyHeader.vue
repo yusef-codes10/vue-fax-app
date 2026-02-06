79a4557cea744de355b23c4fa72b08a8dba9138c
Youcef <youcefcopy@gmail.com>
2026-02-06 16:37:38 +0100

    The header

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,20 @@
+<script setup>
+/*
+CHALLENGE: Turn the provided HTML/CSS mockup into a Vue app that
+           uses three components: Header, Main, and Footer.
+           Make sure you use all the provided CSS!
+*/
+</script>
+
+<template>
+  <header>
+    <!--
+            This image has been commented out for now as it will break
+            until you've learnt how to work with images!
+            <img src="logo_vue_300px.png" alt="vue logo" />
+            -->
+    <h1>Hi, I'm <span class="highlight">Vue</span></h1>
+  </header>
+</template>
+
+<style scoped></style>
